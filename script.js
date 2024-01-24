ec17205e661bd5f382bd27a9296cf2122c60b672
tonjehnord <tonjnord@gmail.com>
2024-01-24 14:27:58 +0100

    Litt css og gjort ferdig javascript (tror jeg)

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let resourcesHTML = ""
+function toggleButton(articleId) {
 
-resources.map(resource => resourcesHTML += `<article>
-    <h2>${resource.category}</h2>
-    <p>${resource.text}</p>
-    <ul>${resource.sources.map(source => 
-      `<li><a href="${source.url}">${source.title}</a></li>`
-    ).join('')}</ul>
-    </article>`)
+    let resourcesHTML = ""
 
+    resources.map(resource => {
+        if (resource.category === articleId) {
+            resourcesHTML += `<article class="hidden" id="${resource.category}">
+            <h2>${resource.category}</h2>
+            <p>${resource.text}</p>
+            <ul>${resource.sources.map(source => 
+            `<li><a href="${source.url}">${source.title}</a></li>`
+            ).join('')}</ul>
+            </article>` 
+        }
+    })
 
-const main = document.getElementsByTagName("main")
-main[0].innerHTML =  resourcesHTML
-       
-    
+    const main = document.getElementsByTagName("main")
+    main[0].innerHTML =  resourcesHTML
+}
+
+toggleButton(`HTML`)
